f1f21d6a0ff41910ce81e253988ef47aeb5bfd6a
foday <foday_s@yahoo.com>
2018-11-12 14:38:30 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function cal() {
+  var a = parseInt(document.querySelector("#value1").value);
+  var b = parseInt(document.querySelector("#value2").value);
+  var op = document.querySelector("#operator").value;
+  var calculate;
+
+  if (op == "add") {
+    calculate = a + b;
+  } else if (op == "min") {
+    var calculate = a - b;
+  } else if (op == "div") {
+    calculate = a / b;
+  } else if (op == "mul") {
+    calculate = a * b;
+  }
+
+  document.querySelector("#result").innerHTML = calculate;
+
+  //console.log(calculate);
+}
